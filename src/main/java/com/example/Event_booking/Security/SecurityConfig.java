63137f186934dd345c198bb9253a1bdab3d8a778
Yatharth Singh <singhyatharthx29@gmail.com>
2026-08-27 10:26:42 +0530

    fixed some seat related issues

--- src/main/java/com/example/Event_booking/Security/SecurityConfig.java
+++ src/main/java/com/example/Event_booking/Security/SecurityConfig.java
@@ -69,7 +69,8 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of(
-                "http://localhost:5173"
+                "http://localhost:5173",
+                "https://eventforgeapp.onrender.com"
         ));
 
         configuration.setAllowedMethods(List.of(
